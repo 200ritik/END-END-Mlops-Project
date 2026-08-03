6d98013a00382ca17c2899eac0b1183d49df77f7
200ritik <ritiksharma7141@gmail.com>
2026-08-03 23:35:21 +0530

    project struct ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='src',
+    version='0.0.1',
+    author = "Ritik Sharma",
+    author_email="rs9816467714@gmail.com",
+    packages=find_packages()    
+    
+)
