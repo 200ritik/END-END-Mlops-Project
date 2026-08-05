ed9b22d509e7ed2d1d1f1a93a9d7dbb5d2d2315d
200ritik <ritiksharma7141@gmail.com>
2026-08-06 02:22:59 +0530

    data ingestion_done

--- demo.py
+++ demo.py
@@ -1,9 +1,14 @@
-from src.logger import logging
-from src.exception import MyException
-import sys
-try:
-    a=1/0
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
-logging.debug("This is a debug message")
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+# try:
+#     a=1/0
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+# logging.debug("This is a debug message")\
+    
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
